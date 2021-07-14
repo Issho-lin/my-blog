01e76ad22c97da004fc78dd882f1c6ef8c059a37
10828 <lqb@addcn.com>
2021-07-14 10:49:31 +0800

    重学js系列

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const themeConfig = require('./config/theme')
 
 module.exports = {
     title: 'Issho lqb',
-    description: '林琦彬 linqibin Issho web前端 个人网站',
+    description: 'web前端个人博客',
     head: [
         ['link', { rel: 'icon', href: '/assets/image/favicon.jpg' }],
         ['meta', { name: 'referrer', content: 'no-referrer' }]
