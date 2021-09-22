6679c728d5f1d483bd2b37d0b94a903e5ee3703b
林琦彬 <lqb@addcn.com>
2021-09-22 18:17:24 +0800

    重学js系列只红宝书第4章

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,13 +1,4 @@
 module.exports = [
-    // { text: '前端', items: [
-    //     { text: 'JavaScript', link: '/fronted/javascript/' }, 
-    //     { text: 'React', link: '/fronted/react/' },
-    //     { text: 'vue', link: '/fronted/vue/' },
-    //     { text: 'webpack', link: '/fronted/webpack/' },
-    //     { text: 'node', link: '/fronted/node/' },
-    // ] },
-    // { text: '算法', link: '/arithmetic/' },
-    // { text: '读书', link: '/books/' },
     { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
     { text: 'Github', link: 'https://github.com/Issho-lin' }
 ]
